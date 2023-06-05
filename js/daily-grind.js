cb3096d1623db9cdfa03adc188016db34f879bb8
Replit user <>
2023-06-05 22:26:32 +0000

    Changes Added

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -7,20 +7,45 @@
     day - the day of the week
     desc - description of the coffee
 
-
-
-
-
-
 */
 let myDate = new Date();
 
 let today = myDate.getDay();
 
+let coffee = "";
+
+function coffeeTemplate(coffee){
+
+    return`
+  
+    <p>
+    <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+    <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
+    
+   `; 
+  
+  }
+
+
+
 //today = 3;
 
 switch(today){
 
+  case 1:
+    today = "Monday";
+
+    coffee = {
+        name: "Bubble Tea",
+        pic: "images/bubble-tea.jpg",
+        alt: "a picture of a bubble tea",
+        color: "pink",
+        day: "Monday",
+        desc: `I love me some bubble tea!`
+    };
+    
+    break;
+    
     case 2:
          today = "Taco Tuesdays";
     break;
@@ -32,13 +57,7 @@ switch(today){
    default:
       today = "Day is unknown";
 }
-let coffee = {
-    name: "Bubble Tea",
-    pic: "images/bubble-tea.jpg",
-    alt: "a picture of a bubble tea",
-    color: "pink",
-    day: "Wednesday",
-    desc: `I love me some bubble tea!`
-}
+
+document.getElementById("coffee-cup").innerHTML  = coffeeTemplate(coffee);
 
 console.log(coffee);
